d1b8edd18486af22e1863f857df3ce5730283638
Rémy LESCALLIER <lescallier1@gmail.com>
2019-03-03 22:05:18 +0100

    Init serializer service

---
+++ src/Service/SerializerService.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Coosos\VersionWorkflowBundle\Service;
+
+use Coosos\VersionWorkflowBundle\Normalizer\VersionWorkflowNormalize;
+use Symfony\Component\Serializer\Encoder\JsonEncoder;
+use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
+use Symfony\Component\Serializer\SerializerInterface;
+
+class SerializerService implements SerializerInterface
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function serialize($data, $format, array $context = [])
+    {
+        dump($this->getSerializer()->serialize($data, $format, $context));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function deserialize($data, $type, $format, array $context = [])
+    {
+    }
+
+    protected function getSerializer()
+    {
+        return new \Symfony\Component\Serializer\Serializer(
+            [new VersionWorkflowNormalize(), new ObjectNormalizer()],
+            [new JsonEncoder()]
+        );
+    }
+}
