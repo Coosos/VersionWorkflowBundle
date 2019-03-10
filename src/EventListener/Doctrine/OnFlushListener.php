6050163feeae7ae517a6ec14bb1adc32c5919b4a
Rémy Lescallier <lescallier1@gmail.com>
2019-03-10 03:48:22 +0100

    Update onflush

--- src/EventListener/Doctrine/OnFlushListener.php
+++ src/EventListener/Doctrine/OnFlushListener.php
@@ -6,6 +6,7 @@ use Coosos\VersionWorkflowBundle\Model\VersionWorkflowConfiguration;
 use Coosos\VersionWorkflowBundle\Model\VersionWorkflowTrait;
 use Coosos\VersionWorkflowBundle\Utils\ClassContains;
 use Doctrine\ORM\Event\OnFlushEventArgs;
+use Symfony\Component\Workflow\Registry;
 
 /**
  * Class OnFlushListener
@@ -24,19 +25,26 @@ class OnFlushListener
      * @var VersionWorkflowConfiguration
      */
     private $versionWorkflowConfiguration;
+    /**
+     * @var Registry
+     */
+    private $registry;
 
     /**
      * OnFlushListener constructor.
      *
      * @param ClassContains $classContains
      * @param VersionWorkflowConfiguration $versionWorkflowConfiguration
+     * @param Registry $registry
      */
     public function __construct(
         ClassContains $classContains,
-        VersionWorkflowConfiguration $versionWorkflowConfiguration
+        VersionWorkflowConfiguration $versionWorkflowConfiguration,
+        Registry $registry
     ) {
         $this->classContains = $classContains;
         $this->versionWorkflowConfiguration = $versionWorkflowConfiguration;
+        $this->registry = $registry;
     }
 
     /**
@@ -61,7 +69,31 @@ class OnFlushListener
                     if ($this->hasVersionWorkflowTrait($scheduledEntity)
                         && !is_null($scheduledEntity->getWorkflowName())
                         && $this->hasConfigurationByWorkflowName($scheduledEntity)) {
+                        $currentPlace = $this->getCurrentPlace($scheduledEntity);
+                        $autoMerge = false;
 
+                        if ($currentPlace) {
+                            if (is_array($currentPlace)) {
+                                foreach ($currentPlace as $status => $value) {
+                                    if ($this->isAutoMerge($scheduledEntity, $status)) {
+                                        $autoMerge = true;
+                                        break;
+                                    }
+                                }
+                            } elseif (is_string($currentPlace)) {
+                                if ($this->isAutoMerge($scheduledEntity, $currentPlace)) {
+                                    $autoMerge = true;
+                                }
+                            }
+                        }
+
+                        if (!$autoMerge) {
+//                            $this->detachRecursive($args, $scheduledEntity);
+                        }
+
+                        dump($autoMerge);
+                        dump($this->getCurrentPlace($scheduledEntity));
+                        die;
                     }
                 }
             }
@@ -88,5 +120,34 @@ class OnFlushListener
     public function hasConfigurationByWorkflowName($model)
     {
         return $this->versionWorkflowConfiguration->hasConfigurationByWorkflowName($model->getWorkflowName());
+    }
+
+    /**
+     * @param VersionWorkflowTrait $model
+     * @return mixed|null
+     */
+    public function getCurrentPlace($model)
+    {
+        $workflow = $this->registry->get($model, $model->getWorkflowName());
+
+        $getterMethod = $this->classContains->getGetterMethod($model, $workflow->getMarkingStore()->getProperty());
+
+        if ($getterMethod) {
+            return $model->{$getterMethod}();
+        }
+
+        return null;
+    }
+
+    /**
+     * @param VersionWorkflowTrait $model
+     * @return bool
+     */
+    public function isAutoMerge($model, $place)
+    {
+        return $this->versionWorkflowConfiguration->isAutoMerge(
+            $model->getWorkflowName(),
+            $place
+        );
     }
 }
