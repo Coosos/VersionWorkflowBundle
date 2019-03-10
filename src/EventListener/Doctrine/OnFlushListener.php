9a111116618504f7aa33586abd928f0278faa59f
Rémy Lescallier <lescallier1@gmail.com>
2019-03-10 17:38:55 +0100

    Restore detach entity

--- src/EventListener/Doctrine/OnFlushListener.php
+++ src/EventListener/Doctrine/OnFlushListener.php
@@ -5,7 +5,10 @@ namespace Coosos\VersionWorkflowBundle\EventListener\Doctrine;
 use Coosos\VersionWorkflowBundle\Model\VersionWorkflowConfiguration;
 use Coosos\VersionWorkflowBundle\Model\VersionWorkflowTrait;
 use Coosos\VersionWorkflowBundle\Utils\ClassContains;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Event\OnFlushEventArgs;
+use Doctrine\ORM\PersistentCollection;
 use Symfony\Component\Workflow\Registry;
 
 /**
@@ -16,19 +19,29 @@ use Symfony\Component\Workflow\Registry;
  */
 class OnFlushListener
 {
+    const PROPERTY_DETACH = 'workflowDetach';
+
+    const VERSION_WORKFLOW_PROPERTY = 'versionWorkflow';
+
     /**
      * @var ClassContains
      */
-    private $classContains;
+    protected $classContains;
 
     /**
      * @var VersionWorkflowConfiguration
      */
-    private $versionWorkflowConfiguration;
+    protected $versionWorkflowConfiguration;
+
     /**
      * @var Registry
      */
-    private $registry;
+    protected $registry;
+
+    /**
+     * @var array
+     */
+    protected $detachDeletionsHash;
 
     /**
      * OnFlushListener constructor.
@@ -45,11 +58,16 @@ class OnFlushListener
         $this->classContains = $classContains;
         $this->versionWorkflowConfiguration = $versionWorkflowConfiguration;
         $this->registry = $registry;
+        $this->detachDeletionsHash = [];
     }
 
     /**
+     * Detach entity if is vworkflow (not merge)
+     *
      * @param OnFlushEventArgs $args
+     *
      * @throws \ReflectionException
+     * @throws \Doctrine\ORM\ORMException
      */
     public function onFlush(OnFlushEventArgs $args)
     {
@@ -88,14 +106,122 @@ class OnFlushListener
                         }
 
                         if (!$autoMerge) {
-//                            $this->detachRecursive($args, $scheduledEntity);
+                            $this->detachRecursive($args, $scheduledEntity);
                         }
-
-                        dump($autoMerge);
-                        dump($this->getCurrentPlace($scheduledEntity));
-                        die;
                     }
                 }
+            }
+            if ($scheduledType === 'entityDeletions' || $scheduledType === 'collectionDeletions') {
+                foreach ($item as $scheduledEntity) {
+                    $this->detachDeletionsHash = [];
+                    $detachDeletions = $this->checkDetachDeletionsRecursive($args, $scheduledEntity);
+                    if ($detachDeletions && !$scheduledEntity instanceof PersistentCollection) {
+                        $args->getEntityManager()->persist($scheduledEntity);
+                        $this->detachRecursive($args, $scheduledEntity);
+                    }
+
+                    if ($detachDeletions && $scheduledEntity instanceof PersistentCollection) {
+                        $mapping = $scheduledEntity->getMapping();
+                        $mapping['orphanRemoval'] = false;
+                        $scheduledEntity->setOwner($scheduledEntity->getOwner(), $mapping);
+                    }
+                }
+            }
+        }
+
+        if ($unitOfWork->getScheduledEntityInsertions()) {
+            foreach ($unitOfWork->getScheduledEntityInsertions() as $entityInsertion) {
+                if (property_exists($entityInsertion, self::PROPERTY_DETACH)
+                    && $entityInsertion->{self::PROPERTY_DETACH}) {
+                    $args->getEntityManager()->detach($entityInsertion);
+                }
+            }
+        }
+    }
+
+    /**
+     * Check if relation use object detached
+     *
+     * @param OnFlushEventArgs           $args
+     * @param VersionWorkflowTrait|mixed $entity
+     *
+     * @return bool
+     */
+    protected function checkDetachDeletionsRecursive(OnFlushEventArgs $args, $entity)
+    {
+        if (is_null($entity)) {
+            return false;
+        }
+
+        if (property_exists($entity, self::PROPERTY_DETACH) && $entity->{self::PROPERTY_DETACH}) {
+            return true;
+        }
+
+        if (in_array(spl_object_hash($entity), $this->detachDeletionsHash)) {
+            return false;
+        }
+
+        if ($entity instanceof PersistentCollection) {
+            $isDetach = $this->checkDetachDeletionsRecursive($args, $entity->getOwner());
+            if ($isDetach) {
+                return true;
+            }
+        }
+
+        $meta = $args->getEntityManager()->getClassMetadata(get_class($entity));
+        foreach ($meta->getAssociationMappings() as $fieldName => $associationMapping) {
+            if (!$entity->{'get' . ucfirst($fieldName)}() instanceof Collection) {
+                $isDetach = $this->checkDetachDeletionsRecursive($args, $entity->{'get' . ucfirst($fieldName)}());
+                if ($isDetach) {
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+
+    /**
+     * Recursive detach
+     *
+     * @param OnFlushEventArgs           $args
+     * @param VersionWorkflowTrait|mixed $entity
+     */
+    protected function detachRecursive(OnFlushEventArgs $args, $entity)
+    {
+        $args->getEntityManager()->detach($entity);
+        $entity->{self::PROPERTY_DETACH} = true;
+        $classMetaData = $args->getEntityManager()->getClassMetadata(get_class($entity));
+        foreach ($classMetaData->getAssociationMappings() as $key => $associationMapping) {
+            if ($entity->{'get' . ucfirst($key)}() instanceof PersistentCollection) {
+                /** @var PersistentCollection $getCollectionMethod */
+                $getCollectionMethod = $entity->{'get' . ucfirst($key)}();
+                foreach ($getCollectionMethod as $item) {
+                    if (property_exists($item, self::PROPERTY_DETACH) && $item->{self::PROPERTY_DETACH}) {
+                        continue;
+                    }
+
+                    $this->detachRecursive($args, $item);
+
+                    continue;
+                }
+
+                /** @var PersistentCollection $tags */
+                $mapping = $getCollectionMethod->getMapping();
+                $mapping['isOwningSide'] = false;
+                $getCollectionMethod->setOwner($entity, $mapping);
+            } elseif (!$entity->{'get' . ucfirst($key)}() instanceof ArrayCollection
+                && $key !== self::VERSION_WORKFLOW_PROPERTY) {
+                $item = $entity->{'get' . ucfirst($key)}();
+                if (!is_null($item)) {
+                    if (property_exists($item, self::PROPERTY_DETACH) && $item->{self::PROPERTY_DETACH}) {
+                        continue;
+                    }
+
+                    $this->detachRecursive($args, $item);
+                }
+
+                continue;
             }
         }
     }
@@ -108,7 +234,7 @@ class OnFlushListener
      * @return bool
      * @throws \ReflectionException
      */
-    public function hasVersionWorkflowTrait($model)
+    protected function hasVersionWorkflowTrait($model)
     {
         return $this->classContains->hasTrait($model, VersionWorkflowTrait::class);
     }
@@ -117,7 +243,7 @@ class OnFlushListener
      * @param VersionWorkflowTrait $model
      * @return bool
      */
-    public function hasConfigurationByWorkflowName($model)
+    protected function hasConfigurationByWorkflowName($model)
     {
         return $this->versionWorkflowConfiguration->hasConfigurationByWorkflowName($model->getWorkflowName());
     }
@@ -126,7 +252,7 @@ class OnFlushListener
      * @param VersionWorkflowTrait $model
      * @return mixed|null
      */
-    public function getCurrentPlace($model)
+    protected function getCurrentPlace($model)
     {
         $workflow = $this->registry->get($model, $model->getWorkflowName());
 
@@ -141,9 +267,10 @@ class OnFlushListener
 
     /**
      * @param VersionWorkflowTrait $model
+     * @param string               $place
      * @return bool
      */
-    public function isAutoMerge($model, $place)
+    protected function isAutoMerge($model, $place)
     {
         return $this->versionWorkflowConfiguration->isAutoMerge(
             $model->getWorkflowName(),
